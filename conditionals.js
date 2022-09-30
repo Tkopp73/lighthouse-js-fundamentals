3ed8ee5e98da10ac3c854fd6225a7742efbd9952
Tim Kopplin <timkopplin@hotmail.com>
2022-09-30 21:43:22 +0000

    adding changes

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,4 @@
+/*
 const temperature = 15;
 
 if (temperature < 0) {
@@ -9,20 +10,41 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+*/
 
-
+/*
 const isCitizen = true;
 const age = 26;
 
 if (isCitizen && age > 18) {
   console.log("You are able to vote.");
 }
+*/
 
-
+/*
 let raining = false;
 
 if (!raining) {
   console.log("Leave your umbrella at home!");
 } else {
   console.log("Better bring an umbrella!");
+}
+*/
+
+/* let solution = 1;
+for (let x = 1; x <= 12; x++){
+  solution *= x;
+}
+console.log(solution);
+*/
+
+let rows = "";
+let seats = "";
+
+for (let x = 0; x < 26; x++){
+  for (let y = 0; y < 100; y++){
+    let rows = x;
+    let seats = y;
+    console.log(rows + "-" + seats);
+  }
 }
